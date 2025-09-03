b8d2e7f3e66427b2488a5ec32c335f9301642675
Neda <neda@Nedas-MacBook-Air.local>
2025-09-04 03:19:40 +0330

    rendering data

--- js/main.js
+++ js/main.js
@@ -1,27 +1,49 @@
-/*
-<div class="product col-lg-4 col-md-6 mb-4">
+
+
+const data = fetch("http://localhost:3000/products")
+.then(response =>{
+	if (!response.ok){
+		throw new Error("Network response was not ok " + response.statusText);
+		
+	}
+	return response.json();
+})
+.then(data => {
+	const container = document.getElementById("product-list");
+	container.innerHTML = data
+	.map(
+		(product, index) => `
+		<div class="product col-lg-4 col-md-6 mb-4">
 		<div class="card h-100">
 			<a href="#"
 				><img
 					class="card-img-top"
-					src="ادرس عکس محصول"
+					src=${product.image}
 					alt="عنوان محصول"
 			/></a>
 			<div class="card-body">
 				<h4 class="card-title">
-				    عنوان محصول
+				${product.title}
 				</h4>
-				<h5 class="product-price">قیمت محصول تومان</h5>
+				<h5 class="product-price">${Number(product.price).toLocaleString("en-US")} تومان</h5>
 				<p class="card-text">
-				    توضیحات محصول
+				    ${product.description}
 				</p>
 			</div>
 			<div class="card-footer">
-				<button class="btn btn-light add-to-cart" data-product-id="ایدی محصول">
+				<button class="btn btn-light add-to-cart" data-product-id="${product.id}">
 					افزودن به سبد خرید
 				</button>
 			</div>
 		</div>
 	</div>
-*/
+		`
+	)
+	.join("")
+	console.log("fetched data:", data)
+})
+.catch(error =>{
+	console.error("fetch error:", error);
+});
+
 
